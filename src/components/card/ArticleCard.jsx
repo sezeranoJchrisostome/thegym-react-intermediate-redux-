33d4590ec31d2d5bcc0986d3979e13672d02ff1e
sezerano jean chrysostome <sezeranochrisostom123@gmail.com>
2023-04-09 21:43:14 +0200

    fix: adds fixed height for article card

--- src/components/card/ArticleCard.jsx
+++ src/components/card/ArticleCard.jsx
@@ -2,10 +2,10 @@ export default function ArticleCard({ article, handleRedirect = () => null }) {
   return (
     <div
       onClick={() => handleRedirect(article)}
-      className="card bg-base-100 shadow-xl image-full cursor-pointer"
+      className="card bg-base-100 shadow-xl image-full cursor-pointer relative overflow-hidden min-h-[200px] max-h-[200px]"
     >
       <figure>
-        <img src={article?.urlToImage} alt="Shoes" />
+        <img className="" src={article?.urlToImage} alt="Shoes" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{article.source?.name}</h2>
